2f28c9c1c4ce99e5d82b76898a3c348326ef3575
MelodyCube <1152a0xy425@gmail.com>
2025-11-03 08:37:35 -0500

    tool modification
    small

---
+++ kubejs/server_scripts/tags/item.js
@@ -0,0 +1,8 @@
+ServerEvents.tags('item', event => {
+
+    event.add('kubejs:bone_material', [
+        'minecraft:bone_meal',
+        'biomancy:bone_fragments'
+    ])
+
+})
--- kubejs/startup_scripts/custom_content/tools.js
+++ kubejs/startup_scripts/custom_content/tools.js
@@ -5,7 +5,7 @@ ItemEvents.toolTierRegistry(event => {
     tier.attackDamageBonus = 1.5
     tier.level = 1
     tier.enchantmentValue = 20
-    tier.repairIngredient = 'minecraft:bone'
+    tier.repairIngredient = '#kubejs:bone_material'
   })
 
   event.add('living', tier => {
@@ -14,24 +14,46 @@ ItemEvents.toolTierRegistry(event => {
     tier.attackDamageBonus = 2
     tier.level = 1
     tier.enchantmentValue = 22
-    tier.repairIngredient = 'minecraft:bone'
+    tier.repairIngredient = 'biomancy:flesh_bits'
   })
 })
 
 StartupEvents.registry('item', event => {
   event.create('bone_sword', 'sword').tier('bone')
+    .tag('minecraft:swords')
+    .tag('forge:tools/bone')
   event.create('bone_pickaxe', 'pickaxe').tier('bone')
+    .tag('minecraft:pickaxes')
+    .tag('forge:tools/bone')
   event.create('bone_shovel', 'shovel').tier('bone')
+    .tag('minecraft:shovels')
+    .tag('forge:tools/bone')
   event.create('bone_axe', 'axe').tier('bone')
+    .tag('minecraft:axes')
+    .tag('forge:tools/bone')
   event.create('bone_hoe', 'hoe').tier('bone')
+    .tag('minecraft:hoes')
+    .tag('forge:tools/bone')
   event.create('bone_knife', 'farmersdelight:knife').tier('bone')
     .tag('forge:tools/knives')
+    .tag('forge:tools/bone')
 
   event.create('living_sword', 'sword').tier('living')
+    .tag('minecraft:swords')
+    .tag('forge:tools/living')
   event.create('living_pickaxe', 'pickaxe').tier('living')
+    .tag('minecraft:pickaxes')
+    .tag('forge:tools/living')
   event.create('living_shovel', 'shovel').tier('living')
+    .tag('minecraft:shovels')
+    .tag('forge:tools/living')
   event.create('living_axe', 'axe').tier('living')
+    .tag('minecraft:axes')
+    .tag('forge:tools/living')
   event.create('living_hoe', 'hoe').tier('living')
+    .tag('minecraft:hoes')
+    .tag('forge:tools/living')
   event.create('living_knife', 'farmersdelight:knife').tier('living')
     .tag('forge:tools/knives')
+    .tag('forge:tools/living')
 })
