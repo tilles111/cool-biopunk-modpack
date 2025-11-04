7c2df555d0292535c19384ecd6e9ad30e1e5f317
tilles111 <59595469+tilles111@users.noreply.github.com>
2025-11-03 16:11:27 -0800

    Tools (lots of them)

--- kubejs/startup_scripts/custom_content/tools.js
+++ kubejs/startup_scripts/custom_content/tools.js
@@ -16,11 +16,46 @@ ItemEvents.toolTierRegistry(event => {
     tier.enchantmentValue = 20
     tier.repairIngredient = 'biomancy:flesh_bits'
   })
+  event.add('biobrass', tier => {
+    tier.uses = 500
+    tier.speed = 7
+    tier.attackDamageBonus = 2.5
+    tier.level = 2
+    tier.enchantmentValue = 18
+    tier.repairIngredient = '#kubejs:bone_materials'
+  })
+
+  event.add('living_biobrass', tier => {
+    tier.uses = 500
+    tier.speed = 7
+    tier.attackDamageBonus = 2.5
+    tier.level = 2
+    tier.enchantmentValue = 18
+    tier.repairIngredient = 'biomancy:flesh_bits'
+  })
+  
+  event.add('chitinsteel', tier => {
+    tier.uses = 1750
+    tier.speed = 9
+    tier.attackDamageBonus = 3.5
+    tier.level = 3
+    tier.enchantmentValue = 15
+    tier.repairIngredient = '#kubejs:bone_materials'
+  })
+
+  event.add('living_chitinsteel', tier => {
+    tier.uses = 1750
+    tier.speed = 9
+    tier.attackDamageBonus = 3.5
+    tier.level = 3
+    tier.enchantmentValue = 15
+    tier.repairIngredient = 'biomancy:flesh_bits'
+  })
 })
 
 StartupEvents.registry('item', event => {
   event.create('living_tool_smithing_template')
-  
+
   event.create('bone_sword', 'sword').tier('bone')
     .tag('minecraft:swords')
     .tag('forge:tools/bone')
@@ -58,4 +93,80 @@ StartupEvents.registry('item', event => {
   event.create('living_bone_knife', 'farmersdelight:knife').tier('living_bone')
     .tag('forge:tools/knives')
     .tag('forge:tools/living_bone')
+
+  event.create('biobrass_sword', 'sword').tier('biobrass')
+    .tag('minecraft:swords')
+    .tag('forge:tools/biobrass')
+  event.create('biobrass_pickaxe', 'pickaxe').tier('biobrass')
+    .tag('minecraft:pickaxes')
+    .tag('forge:tools/biobrass')
+  event.create('biobrass_shovel', 'shovel').tier('biobrass')
+    .tag('minecraft:shovels')
+    .tag('forge:tools/biobrass')
+  event.create('biobrass_axe', 'axe').tier('biobrass')
+    .tag('minecraft:axes')
+    .tag('forge:tools/biobrass')
+  event.create('biobrass_hoe', 'hoe').tier('biobrass')
+    .tag('minecraft:hoes')
+    .tag('forge:tools/biobrass')
+  event.create('biobrass_knife', 'farmersdelight:knife').tier('biobrass')
+    .tag('forge:tools/knives')
+    .tag('forge:tools/biobrass')
+
+  event.create('living_biobrass_sword', 'sword').tier('living_biobrass')
+    .tag('minecraft:swords')
+    .tag('forge:tools/living_biobrass')
+  event.create('living_biobrass_pickaxe', 'pickaxe').tier('living_biobrass')
+    .tag('minecraft:pickaxes')
+    .tag('forge:tools/living_biobrass')
+  event.create('living_biobrass_shovel', 'shovel').tier('living_biobrass')
+    .tag('minecraft:shovels')
+    .tag('forge:tools/living_biobrass')
+  event.create('living_biobrass_axe', 'axe').tier('living_biobrass')
+    .tag('minecraft:axes')
+    .tag('forge:tools/living_biobrass')
+  event.create('living_biobrass_hoe', 'hoe').tier('living_biobrass')
+    .tag('minecraft:hoes')
+    .tag('forge:tools/living_biobrass')
+  event.create('living_biobrass_knife', 'farmersdelight:knife').tier('living_biobrass')
+    .tag('forge:tools/knives')
+    .tag('forge:tools/living_biobrass')
+
+  event.create('chitinsteel_sword', 'sword').tier('chitinsteel')
+    .tag('minecraft:swords')
+    .tag('forge:tools/chitinsteel')
+  event.create('chitinsteel_pickaxe', 'pickaxe').tier('chitinsteel')
+    .tag('minecraft:pickaxes')
+    .tag('forge:tools/chitinsteel')
+  event.create('chitinsteel_shovel', 'shovel').tier('chitinsteel')
+    .tag('minecraft:shovels')
+    .tag('forge:tools/chitinsteel')
+  event.create('chitinsteel_axe', 'axe').tier('chitinsteel')
+    .tag('minecraft:axes')
+    .tag('forge:tools/chitinsteel')
+  event.create('chitinsteel_hoe', 'hoe').tier('chitinsteel')
+    .tag('minecraft:hoes')
+    .tag('forge:tools/chitinsteel')
+  event.create('chitinsteel_knife', 'farmersdelight:knife').tier('chitinsteel')
+    .tag('forge:tools/knives')
+    .tag('forge:tools/chitinsteel')
+
+  event.create('living_chitinsteel_sword', 'sword').tier('living_chitinsteel')
+    .tag('minecraft:swords')
+    .tag('forge:tools/living_chitinsteel')
+  event.create('living_chitinsteel_pickaxe', 'pickaxe').tier('living_chitinsteel')
+    .tag('minecraft:pickaxes')
+    .tag('forge:tools/living_chitinsteel')
+  event.create('living_chitinsteel_shovel', 'shovel').tier('living_chitinsteel')
+    .tag('minecraft:shovels')
+    .tag('forge:tools/living_chitinsteel')
+  event.create('living_chitinsteel_axe', 'axe').tier('living_chitinsteel')
+    .tag('minecraft:axes')
+    .tag('forge:tools/living_chitinsteel')
+  event.create('living_chitinsteel_hoe', 'hoe').tier('living_chitinsteel')
+    .tag('minecraft:hoes')
+    .tag('forge:tools/living_chitinsteel')
+  event.create('living_chitinsteel_knife', 'farmersdelight:knife').tier('living_chitinsteel')
+    .tag('forge:tools/knives')
+    .tag('forge:tools/living_chitinsteel')
 })
