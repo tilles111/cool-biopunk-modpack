b3fbd746b9915b04039716f03b79622fa9a814b4
MelodyCube <1152a0xy425@gmail.com>
2025-11-12 14:05:57 -0500

    wanted to do more
    oops

--- kubejs/server_scripts/tags/block.js
+++ kubejs/server_scripts/tags/block.js
@@ -1,5 +1,9 @@
 ServerEvents.tags('block', event => {
 
+    event.add('minecraft:mineable/hoe', [
+        'architects_palette:entrails'
+    ])
+
     event.add('forge:mineable/knife', [
         '#minecraft:mineable/hoe'
     ])
