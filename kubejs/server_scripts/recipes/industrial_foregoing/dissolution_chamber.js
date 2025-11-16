03f9761beafdf60bdf4f834bf0affa8b5c79cbf9
tilles111 <59595469+tilles111@users.noreply.github.com>
2025-11-15 20:10:53 -0800

    Dragon organ recipes (I still need to do mana reactor)

--- kubejs/server_scripts/recipes/industrial_foregoing/dissolution_chamber.js
+++ kubejs/server_scripts/recipes/industrial_foregoing/dissolution_chamber.js
@@ -18,4 +18,10 @@ ServerEvents.recipes(event => {
         event.recipes.industrialforegoing.dissolution_chamber([`chestcavity:rotten_${organ}`, 'chestcavity:raw_toxic_organ_meat', 'chestcavity:raw_toxic_organ_meat'], Fluid.of('industrialforegoing:meat', 500), `chestcavity:insect_${organ}`, 400)
         event.recipes.industrialforegoing.dissolution_chamber([`chestcavity:rotten_${organ}`, 'chestcavity:insect_muscle', 'chestcavity:insect_muscle'], Fluid.of('industrialforegoing:meat', 500), `chestcavity:insect_${organ}`, 400)
     })
+    const dragon_organs = ['heart', 'lung', 'liver', 'kidney', 'spleen', 'appendix', 'rib', 'spine']
+    dragon_organs.forEach(organ => {
+        event.recipes.industrialforegoing.dissolution_chamber([`chestcavity:${organ}`, 'chestcavity:raw_dragon_organ_meat'], Fluid.of('thermal:ender', 250), `chestcavity:dragon_${organ}`, 1800)
+        event.recipes.industrialforegoing.dissolution_chamber([`chestcavity:${organ}`, 'biomancy:malignant_flesh', 'biomancy:malignant_flesh', 'biomancy:malignant_flesh', 'biomancy:malignant_flesh'], Fluid.of('thermal:ender', 1000), `chestcavity:dragon_${organ}`, 1800)
+    })
+    event.recipes.industrialforegoing.dissolution_chamber(['chestcavity:muscle', 'biomancy:malignant_flesh'], Fluid.of('thermal:ender', 125), 'chestcavity:dragon_muscle', 1800)
 })
